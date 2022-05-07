a5f6505a14b39f70a38611387e518d2b63327e4b
Nikita Ivanov <darkthefirst@gmail.com>
2022-05-07 14:30:51 +0300

    dev доступ к системе

--- Middlewares/auth.js
+++ Middlewares/auth.js
@@ -5,28 +5,46 @@ const UserModel = require('../Models').user;
 module.exports = async function (req, res, next) {
 	try {
 		const authorizationHeader = req.headers.authorization;
+
 		if (!authorizationHeader) {
 			return next(ApiError.UnauthorizedError());
 		}
 
 		const accessToken = authorizationHeader.split(' ')[1];
-		if (!accessToken) {
+		if (!accessToken && process.env.NODE_ENV != 'development') {
 			return next(ApiError.UnauthorizedError());
 		}
 
-		const validatedToken = tokenService.validateAccessToken(accessToken);
-		if (!validatedToken) {
+		let validatedToken = tokenService.validateAccessToken(accessToken);
+		if (!validatedToken && process.env.NODE_ENV != 'development') {
 			return next(ApiError.UnauthorizedError());
 		}
 
-		const { userID } = validatedToken;
+		let userID = null;
+		if (process.env.NODE_ENV == 'development' && !validatedToken) {
+			let securityCheck = authorizationHeader.split('__');
+			if (securityCheck[1] == '1dadqwe$$131#') {
+				userID = securityCheck[0];
+				validatedToken = { userID };
+			}
+		} else {
+			userID = validatedToken.userID;
+		}
+
 		const userDTO = await UserModel.findOne({
 			where: { userID },
 			attributes: ['status', 'regionID', 'allowedRegions'],
 		});
 
+		if (!userDTO) {
+			return next(ApiError.UnauthorizedError());
+		}
+
 		const userAllowedRegions = JSON.parse(userDTO.allowedRegions);
-		if (!userAllowedRegions.includes(parseInt(userDTO.regionID))) {
+		if (
+			!['admin', 'federal'].includes(userDTO.status) &&
+			!userAllowedRegions.includes(parseInt(userDTO.regionID))
+		) {
 			return next(ApiError.Forbidden());
 		}
 
